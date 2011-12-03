e8e1e42ce68a0752ac5f024a6dbcaeaa8f459562
Ivan Lukyanets <lukyanets.ivan@gmail.com>
2011-12-03 18:21:39 +0600

    add docs
    some fix browse1.py
    removed the extra lines

--- templatetags/browse1.py
+++ templatetags/browse1.py
@@ -1,20 +1,10 @@
 # -*- coding: utf-8 -*-
 from django import template
-from django.http import HttpResponseRedirect, HttpResponse, Http404
+from django.http import HttpResponse
 from django.conf import settings
-import os, mimetypes, settings
+import os
 
 register = template.Library()
-
-# http://djangosnippets.org/snippets/101/
-def send_file(path, filename = None, mimetype = None):
-    if filename is None: filename = os.path.basename(path)
-    if mimetype is None:
-        mimetype, encoding = mimetypes.guess_type(filename)
-    data = open(path, "rb")
-    response = HttpResponse(data, mimetype=mimetype)
-    response['Content-Disposition'] = 'attachment; filename=%s' % filename
-    return response
 
 @register.inclusion_tag('browse1.html', takes_context = True)
 def browse1(context, path):
@@ -31,12 +21,4 @@ def browse1(context, path):
             pathfull += b + '/' 
             b = {'name': b, 'url': pathfull}
             breadcrumbs.append(b)
-            # download
-            #basename, extension = os.path.splitext(pathfull[:-1])
-            #if extension:
-            #    my_data = os.path.join(settings.ROOT, pathfull[1:-1])
-            #    #return send_file(os.path.join(settings.ROOT, pathfull[1:-1]))
-            #    response = HttpResponse(my_data, mimetype='application/vnd.ms-excel')
-            #    response['Content-Disposition'] = 'attachment; filename=foo.xls'
-            #    return response
     return locals()
